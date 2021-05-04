519a7c1e4a2aaae2fed0a9945c766063c6db87f1
josegarrera <josegarrera@gmail.com>
2021-05-04 08:23:38 -0300

    MVP

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,52 @@
 const express = require('express');
+const task = require('../models/task');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+const Task = require('../models/task');
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.render('index', {
+        tasks,
+    });
 });
+
+router.post('/add', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.redirect('/');
+});
+
+router.get('/turn/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.render('edit', {
+        task,
+    });
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.findOneAndUpdate({
+        _id: id,
+    }, req.body);
+    res.redirect('/');
+})
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.deleteOne({
+        _id: id,
+    });
+    res.redirect('/');
+})
 
 module.exports = router;
